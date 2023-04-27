6ad4be12d65cf2585c6f25831b3398d1eca85232
Pako2425 <patryk.kocinski340@gmail.com>
2023-04-27 17:05:54 +0200

    added log in and register forms. It's only a view, forms data isn't handle. TODO: handle register form data

--- rest/src/main/java/com/patryk/app/rest/Controller/ApiController.java
+++ rest/src/main/java/com/patryk/app/rest/Controller/ApiController.java
@@ -1,10 +1,12 @@
 package com.patryk.app.rest.Controller;
 
 import com.patryk.app.rest.Repository.FilesRepository;
+import com.patryk.app.rest.Repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import com.patryk.app.rest.Model.FileData;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,13 +17,45 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.util.Optional;
 
-@RestController
+@Controller
 public class ApiController {
 
     @Autowired
     FilesRepository filesRepository;
 
+    @Autowired
+    UsersRepository usersRepository;
+
     private final String FOLDER_PATH = "C:/Users/Lenovo/Desktop/files/";
+
+    @GetMapping(value = "/")
+    public String viewHomePage() {
+        return "homePage";
+    }
+
+    @GetMapping(value = "/register")
+    public String viewRegisterPage() {
+        return "registerForm";
+    }
+
+    @GetMapping(value = "/sign_in")
+    public String viewSignInPage() {
+        return "signInForm";
+    }
+
+    @GetMapping(value = "/terms_and_conditions")
+    public String viewTermsAndConditionsPage() {
+        return "termsAndConditions";
+    }
+
+    @GetMapping(value = "/fileSystem/{fileId}", produces = MediaType.IMAGE_JPEG_VALUE)
+    public byte[] downloadImage(@PathVariable Long fileId) throws IOException {
+        Optional<FileData> fileData = filesRepository.findById(fileId);
+        String filePath = fileData.get().getFilePath();
+        byte[] meme = Files.readAllBytes(new File(filePath).toPath());
+
+        return meme;
+    }
 
     @PostMapping(value = "/fileSystem")
     public Long uploadImage(@RequestParam MultipartFile multipartFile) throws IOException {
@@ -35,14 +69,5 @@ public class ApiController {
         multipartFile.transferTo(new File(filePath));
 
         return fileData.getId();
-    }
-
-    @GetMapping(value = "/fileSystem/{fileId}", produces = MediaType.IMAGE_JPEG_VALUE)
-    public byte[] downloadImage(@PathVariable Long fileId) throws IOException {
-        Optional<FileData> fileData = filesRepository.findById(fileId);
-        String filePath = fileData.get().getFilePath();
-        byte[] meme = Files.readAllBytes(new File(filePath).toPath());
-
-        return meme;
     }
 }
